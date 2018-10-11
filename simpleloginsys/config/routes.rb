5b84e9f29f027a07294113fb123f966e65bf5aa5
gomj0207 <rhalswo1234@naver.com>
2018-10-12 01:21:29 +0900

    Update routes.rb

--- simpleloginsys/config/routes.rb
+++ simpleloginsys/config/routes.rb
@@ -5,12 +5,5 @@ Rails.application.routes.draw do
         post 'login'
     end
   end
-  # get 'login' => 'sessions#new'
-  # post 'login' => 'sessions#create'
-  # delete 'logout' => 'sessions#destroy'
-  # get 'home' => 'application#home'
-
-  # get '/simplelogins/login', to: 'simplelogins#login'
-  # post '/simplelogins/login', to: 'simplelogins#login'
 
  end
